7c02be6f382b59c2cb15e7cfffc8dbdbfaa51608
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2022-11-28 21:38:43 +0600

    Update mongoose.js

--- model/mongoose.js
+++ model/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongo().catch(err => console.log(err));
 async function mongo() {
     try{
-        const url = `mongodb+srv://eddie2111:bmwM3GTR.@cluster0.yql3v.mongodb.net/eris`;
+        const url = `mongodb+srv://${process.env.mongooseUser}:${process.env.mongoosePassword}@${process.env.mongooseCLUSTER}/${process.env.mongooseDatabase}`;
         //const url = `mongodb://localhost:27017/eris`;
         await mongoose.connect(url)
                 .then(() => {console.log('connected to mongo');})

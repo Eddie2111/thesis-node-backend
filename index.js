1b3224422b8eac8d055c07bfc155f5ef59970167
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2023-02-12 22:15:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(session(sessionSetting));
 app.use(cookieparser());
+app.use(cors(corsOption));
 
-app.use("/", cors(corsOptions), require('./routes/index'));
-app.use("/recieve", cors(corsOptions), require('./routes/recieve'));
+app.use("/", require('./routes/index'));
+app.use("/recieve", require('./routes/recieve'));
 app.use("/bulk", require('./routes/bulk'));
-app.use("/login", cors(corsOptions), require('./routes/login'));
-app.use("/success", cors(corsOptions), require('./routes/success'));
+app.use("/login", require('./routes/login'));
+app.use("/success", require('./routes/success'));
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
